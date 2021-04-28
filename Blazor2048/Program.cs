36eae0eb74f09710124e25f7001ee74748d068f6
Lewis-Allen <lewisn11@hotmail.co.uk>
2021-04-28 22:23:39 +0100

    Upgrade to .NET 5.

--- Blazor2048/Program.cs
+++ Blazor2048/Program.cs
@@ -15,7 +15,7 @@ namespace Blazor2048
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             await builder.Build().RunAsync();
         }
